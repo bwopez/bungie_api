7497f56ca6481670bb0c6c2befe7be659562a4ee
Matthew <matthewbropez@gmail.com>
2020-08-06 18:39:23 -0400

    Documentation
    Added documentation to all of the main functions.

--- helper_functions.py
+++ helper_functions.py
@@ -43,45 +43,18 @@ def endpoint_to_Response(link):
 
 
 def get_activity_hashes(activities):
+    """
+    Extracts only the hashes out of the list of activities given
+
+    Args:
+        activities (list): The list of activities
+
+    Returns:
+        activity_hashes (list): The list of hashes
+    """
     activity_hashes = []
     for activity in activities:
         activity_hashes.append(activity["activityDetails"]["directorActivityHash"])
 
     # print(activity_hashes)
     return activity_hashes
-
-
-def thing():
-    # I'm just testing stuff here really
-    x_api_key = os.environ.get("bungie_key")
-    HEADERS = {"X-API-Key":x_api_key}
-    # r = requests.get("https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/3/matthuwu/", headers=HEADERS)
-    r = requests.get("https://www.bungie.net/Platform/User/GetMembershipsById/4611686018437293480/2/", headers=HEADERS)
-    json = r.json()
-    response = json["Response"]
-    DMID = response["destinyMemberships"][1]["membershipId"]
-    DMT = response["destinyMemberships"][1]["membershipType"]
-
-    r2 = requests.get(
-        # ?components=100 is needed for getting profiles
-        "https://www.bungie.net/Platform/Destiny2/2/Profile/4611686018437293480/?components=100".format(DMID, DMT), 
-        headers=HEADERS
-    )
-    json2= r2.json()
-
-    activity_list_link = requests.get(
-        "https://www.bungie.net/Platform/Destiny2/2/Account/4611686018437293480/Character/2305843009267529341/Stats/Activities/",
-        headers=HEADERS
-    )
-    activity_list_json = activity_list_link.json()
-    return activity_list_json
-
-    # TODO: okay we got the character information and '?components=' part lets go
-    ## now how do we find entityType
-    # thingy = requests.get("https://www.bungie.net/Platform/Destiny2/Manifest/{entityType}/4148187374/", headers=HEADERS)
-    # thingy_json = thingy.json()
-    # return thingy_json
-
-
-    # r = requests.get("http://www.bungie.net/Platform/Destiny2/2/Profile/4611686018437293480/", headers=HEADERS)
-    # return activity_list_json["Response"]["activities"][0]
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from helper_functions import steam_checker, endpoint_to_Response, get_activity_hashes
+from helper_functions import (steam_checker, endpoint_to_Response, 
+    get_activity_hashes)
 
 import pprint
-# important starter information
 pp = pprint.PrettyPrinter()
 
 
@@ -11,10 +11,21 @@ pp = pprint.PrettyPrinter()
 ## grabbing matthew's bungie membershipId
 # needs bungie.net ID
 def membership_ID(bungie_membership_id):
+    # TODO: maybe refactor this to get just the Response?
+    """
+    Gets just the Bungie Membership ID
+
+    Args:
+        bungie_membership_id (str): The bungie membership ID of the selected user
+
+    Returns:
+        BMID (str): The bungie membership ID of the selected user
+    """
     GBNUID_link = "/User/GetBungieNetUserById/{}/".format(bungie_membership_id)
     GBNUID_Response = endpoint_to_Response(GBNUID_link)
     BMID = GBNUID_Response["membershipId"]
     # pp.pprint(GBNUID_Response)
+    print(type(BMID))
     
     return BMID
 
@@ -23,6 +34,15 @@ def membership_ID(bungie_membership_id):
 ## grabbing membershipType
 # needs bungie membership ID
 def membership_type(bungie_membership_id):
+    """
+    Gets the membership type for the specific bungie member
+
+    Args:
+        bungie_membership_id (str): The specific bungie membership ID of the selected user
+
+    Returns:
+        user_MT (str): The bungie membership type of the selected user
+    """
     MT_link = "/User/GetMembershipsById/{}/254/".format(bungie_membership_id)
     MT_Response = endpoint_to_Response(MT_link)
     ## getting membershipType for matthew
@@ -37,6 +57,16 @@ def membership_type(bungie_membership_id):
 ## testing out searching for a player endpoint
 # needs membership type, display name
 def search_destiny_player(membership_type, display_name):
+    """
+    Searches for the Destiny Player
+
+    Args:
+        membership_type (str): Which console the player is on. 1 = Xbox, 2 = PSN, 3 = Steam, 254 = Bungie
+        display_name (str): The display name of the player
+
+    Returns:
+        SDP_Response (list): The Response from the endpoint
+    """
     SDP_link = "/Destiny2/SearchDestinyPlayer/{}/{}/".format(membership_type, display_name)
     SDP_Response = endpoint_to_Response(SDP_link)
     # pp.pprint(SDP_Response)
@@ -47,10 +77,19 @@ def search_destiny_player(membership_type, display_name):
 ## GET PROFILE
 # needs membership Type, destiny membership id
 def get_profile(membership_type, destiny_membership_id):
-    # solved. needed ?components=100 to get profiles for this endpoint
+    """
+    Gets profile information of a specific member based on console and destiny membership id
+
+    Args:
+        membership_type (str): Which console the player is on. 1 = Xbox, 2 = PSN, 3 = Steam, 254 = Bungie
+        destiny_membership_id (str): The Destiny Membership ID for the player
+
+    Returns:
+        GP_Response (list): The Response from the endpoint
+    """
     GP_link = "/Destiny2/{}/Profile/{}/?components=100".format(membership_type, destiny_membership_id)
     GP_Response = endpoint_to_Response(GP_link)
-    # pp.pprint(GP_Response)
+    pp.pprint(GP_Response)
 
     return GP_Response
 
@@ -58,6 +97,17 @@ def get_profile(membership_type, destiny_membership_id):
 ## getting character
 # needs membershipType, destinyMembershipId, characterId
 def get_character(membership_type, destiny_membership_id, character_id):
+    """
+    Gets the Destiny 2 character information of a player
+
+    Args:
+        membership_type (str): Which console the player is on. 1 = Xbox, 2 = PSN, 3 = Steam, 254 = Bungie
+        destiny_membership_id (str): The Destiny Membership ID for the player
+        character_id (str): The Character ID for the player's specific character
+
+    Returns: 
+        GC_Response (list): The Response from the endpoint
+    """
     # SOLVED: needed "?components=200" for character summary
     GC_link = "/Destiny2/{}/Profile/{}/Character/{}/?components=200".format(membership_type, destiny_membership_id, character_id)
     GC_Response = endpoint_to_Response(GC_link)
@@ -69,6 +119,17 @@ def get_character(membership_type, destiny_membership_id, character_id):
 ## testing out activity history
 # need membershipType, destinyMemberhipId, characterId
 def get_activity_history(membership_type, destiny_membership_id, character_id):
+    """
+    Gets the activity history for a specific Destiny 2 character
+
+    Args:
+        membership_type (str): Which console the player is on. 1 = Xbox, 2 = PSN, 3 = Steam, 254 = Bungie
+        destiny_membership_id (str): The Destiny Membership ID for the player
+        character_id (str) : The Character ID for the player's specific character
+
+    Returns:
+        GAH_Response (list): The Response from the endpoint
+    """
     GAH_link = "/Destiny2/{}/Account/{}/Character/{}/Stats/Activities/".format(membership_type, destiny_membership_id, character_id)
     GAH_Response = endpoint_to_Response(GAH_link)
 
@@ -77,8 +138,19 @@ def get_activity_history(membership_type, destiny_membership_id, character_id):
 
 # need entity type, hash identifier
 def get_destiny_entity_definition(entity_type, hash_identifier):
+    """
+    Gets the Destiny 2 entity definition
+
+    Args:
+        entity_type (str): What kind of entity
+        hash_identifier (str): The hash that the entity has
+
+    Returns:
+        GDED_Response (list): The Response from the endpoint
+    """
     GDED_link = "/Destiny2/Manifest/{}/{}/".format(entity_type, hash_identifier)
     GDED_Response = endpoint_to_Response(GDED_link)
+    pp.pprint(GDED_Response)
 
     return GDED_Response
 
@@ -118,5 +190,5 @@ if __name__ == "__main__":
     # pp.pprint(get_activity_history()["activities"])
     history = get_activity_history(matthewbropez_mt, matthewbropez_dmid, matthewbropez_character_id1)["activities"]
     activity_hashes = get_activity_hashes(history)
-    for activity in activity_hashes:
-        pp.pprint(get_destiny_entity_definition("DestinyActivityDefinition", activity)["displayProperties"])
+    # for activity in activity_hashes:
+    #     pp.pprint(get_destiny_entity_definition("DestinyActivityDefinition", activity)["displayProperties"])
